40271bd5ac1adf8359e7daac447362bcd56d96f5
Naru0003 <nrm.xxx@docomo.ne.jp>
2023-04-05 02:59:37 +0900

    3章FizzBuzz問題の修正

--- PHPjob/3-1/index.php
+++ PHPjob/3-1/index.php
@@ -3,10 +3,10 @@
 for ($a =1; $a <=100; $a++){
   if($a % 3 != 0 && $a % 5 != 0){
     echo "$a<br>";
+  }elseif($a % 3 ==0 && $a % 5 == 0){
+    echo "FizzBuzz!<br>";
   }elseif ($a % 5 == 0){
     echo "Buzz!<br>";
-  }elseif ($a % 3 == 0 && $a % 5 == 0){
-    echo "FizzBuzz!<br>";
   }elseif ($a % 3 == 0){
     echo "Fizz!<br>";
   }
