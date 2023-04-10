f161ca72d4e054158ac4ac3b9936446d8276f84a
Naru0003 <nrm.xxx@docomo.ne.jp>
2023-04-10 12:21:48 +0900

    3章チェックテスト

--- PHPjob/3-4/answer.php
+++ PHPjob/3-4/answer.php
@@ -4,28 +4,28 @@
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $name = $_POST['name'];
 $answers = $_POST['answer'];
-$correct1 = $_POST['correct1'];
-$correct2 = $_POST['correct2'];
-$correct3 = $_POST['correct3'];
-$corrects = ["$correct1","$correct2","$correct3"];
+$correct_answers = explode(",", $_POST['correct_answers']);
 
 
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-function correctAnswer($answers,$corrects){
-  if ($answers == $corrects){
-    print '正解！';
-  }else{
-    print '残念・・・';
+function checkAnswer($answer, $correct_answer) {
+  if ($answer == $correct_answer) {
+    echo "正解！";
+  } else {
+    echo "残念・・・";
   }
 }
+
 ?>
 <p><!--POST通信で送られてきた名前を表示--><?php echo $name ?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?PHP correctAnswer(0,0); ?>
+<?php checkAnswer($answers[0], $correct_answers[0]); ?>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?PHP correctAnswer(1,1); ?>
+<?php checkAnswer($answers[1], $correct_answers[1]); ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?PHP correctAnswer(2,2); ?>
+<?php checkAnswer($answers[2], $correct_answers[2]); ?>
+
+
--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -3,30 +3,28 @@
 <?php
 //POST送信で送られてきた名前を受け取って変数を作成
 $name = $_POST['name'];
-//①画像を参考に問題文の選択肢の配列を作成してください。
+//問題文の選択肢の配列を作成
 $question1 = ["80", "22", "20", "21"];
 $question2 = ["PHP", "Python", "JAVA", "HTML"];
 $question3 = ["join", "select", "insert", "update"];
-//② ①で作成した、配列から正解の選択肢の変数を作成してください
-$correct1 = $question1[0];
-$correct2 = $question2[3];
-$correct3 = $question3[1];
+
+$correct_answers = ['80', 'HTML', 'select'];
 ?>
-<p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $name ?>さん</p>
+
+<p>お疲れ様です<?php echo $name ?>さん</p>
+
 
 <!--フォームの作成 通信はPOST通信で-->
-
 <h2>①ネットワークのポート番号は何番？</h2>
 <form action="answer.php" method="post">
-
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
-
-<?php 
-foreach ($question1 as $value) {?>
-  <input type="radio" name="answer[0]" value="<?php echo $value ?>" />
-  <?php echo $value;
-}
-?>
+  <!--問題のradioボタンを「foreach」を使って作成する-->
+  <?php 
+  foreach ($question1 as $value) {
+    ?>
+    <input type="radio" name="answer[0]" value="<?php echo $value ?>" />
+    <?php echo $value;
+  }
+  ?>
 
 
 <h2>②Webページを作成するための言語は？</h2>
@@ -52,8 +50,6 @@ foreach ($question3 as $value) {?>
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
 <br>
 <input type="hidden" name="name" value="<?php echo $name ?>" />
-<input type="hidden" name="correct1" value="<?php echo $correct1 ?>" />
-<input type="hidden" name="correct2" value="<?php echo $correct2 ?>" />
-<input type="hidden" name="correct3" value="<?php echo $correct3 ?>" />
+<input type="hidden" name="correct_answers" value="<?php echo implode(",", $correct_answers); ?>">
 <input type="submit" value="回答する" />
-</from>
+</form>
